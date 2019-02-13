7d508ba820ee68d815663b0edb4f5af571032f41
Topiltzin <nokoayzack@gmail.com>
2019-02-13 17:47:46 -0600

    first login form

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Section } from '../Common';
+import './index.scss';
 
 const Login = props => {
   return(
-    <section className={ `login` }>
-      <h3>Login</h3>
-    </section>
+    <Section noTitle bgImage={`/image3.jpg`} className={ `login full bgImage` }>
+      <div className="col-xs-12 col-md-6 formFull row">
+        <div className="formFull--header col-xs-12">
+          <p className="formFull--title">Login</p>
+        </div>
+        <div className="formFull--inputs col-xs-12 row center-xs">
+          <input className="col-xs-12" type="text" placeholder="Nombre de Usuario" />
+          <input className="col-xs-12" type="password" placeholder="Contraseña" />
+        </div>
+      </div>
+    </Section>
   );
 };
 
--- src/components/Common/Section.js
+++ src/components/Common/Section.js
@@ -3,9 +3,9 @@ import './index.scss';
 
 const Section = props => {
   return(
-    <section className={`Section row center-xs ${props.className}`}>
-      <h3 className="title col-xs-12">{ props.title }</h3>
-      <div className={`row col-xs-12 col-md-10 center-xs ${props.classNameDiv}`}>
+    <section style={{backgroundImage:`url(${props.bgImage})`}} className={`Section row center-xs ${props.className}`}>
+      <h3 style={{display:props.noTitle ? 'none' : 'initial'}} className="title col-xs-12">{ props.title }</h3>
+      <div style={{paddingTop:props.noTitle ? '7vh' : '0'}} className={`row col-xs-12 col-md-10 center-xs ${props.classNameDiv}`}>
         { props.children }
       </div>
     </section>
