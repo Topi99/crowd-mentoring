5226179c7853802259830a14e992e34e00cd188c
Topiltzin <nokoayzack@gmail.com>
2019-04-02 18:06:37 -0600

    Bug fixed

--- src/components/Reportes/RepAsesorias.js
+++ src/components/Reportes/RepAsesorias.js
@@ -6,95 +6,100 @@ class RepAsesorias extends React.Component {
   constructor(props) {
     super(props);
 
-    this.dataDefault = {
-      labels: [1, 2, 3, 4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31],
-      solicitadas: 0,
-      datasets: [
-        {
-          label: 'Asesorías Solicitadas',
-          backgroundColor: 'rgba(181,93,232,0.2)',
-          borderColor: 'rgba(181,93,232,1)',
-          borderWidth: 1,
-          hoverBackgroundColor: 'rgba(181,93,232,0.4)',
-          hoverBorderColor: 'rgba(181,93,232,1)',
-          data: [0,0,0,0,0,0,0,0,0,0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-        },
-        {
-          label: 'Asesorías Concretadas',
-          backgroundColor: 'rgba(68,144,255,0.2)',
-          borderColor: 'rgba(68,144,255,1)',
-          borderWidth: 1,
-          hoverBackgroundColor: 'rgba(68,144,255,0.4)',
-          hoverBorderColor: 'rgba(68,144,255,1)',
-          data: [0,0,0,0,0,0,0,0,0,0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-        } ,
-        {
-          label: 'Asesorías No Concretadas',
-          backgroundColor: 'rgba(255,99,132,0.2)',
-          borderColor: 'rgba(255,99,132,1)',
-          borderWidth: 1,
-          hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-          hoverBorderColor: 'rgba(255,99,132,1)',
-          data: [0,0,0,0,0,0,0,0,0,0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-        } 
-      ]
-    };
-
-    this.MONTHS = ['enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio', 'agosto', 'septiembre', 'ocubre', 'noviembre', 'diciembre'];
+    this.MONTHS = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Ocubre', 'Noviembre', 'Diciembre'];
 
     this.INITIAL_STATE = {
       total: 1,
       totalMes: 0,
       concretadas: 0,
       canceladas: 0,
-      asesoriasPorMes: { enero: {} },
-      mesSolicitado: 'enero',
+      asesoriasPorMes: { },
+      mesSolicitado: 'Enero',
       solicitadasPorMes: [0,0,0,0,0,0,0,0,0,0,0,0]
     };
     
     this.state = { ...this.INITIAL_STATE };
 
-    props.firebase.db.collection('asesorias').onSnapshot(doc => {
+    this.getRange(this.state.mesSolicitado);
+
+    props.firebase.db.collection('asesorias').get().then(doc => {
       this.setState({ total: doc.size, canceladas: 0, totalMes: 0, concretadas: 0 })
-      // console.log(doc);
-      let date;
-      doc.forEach(asesoria => {
-        let dataMonth = [];
-        date = new Date(asesoria.data().fechaDeSolicitud.seconds*1000);
-        dataMonth = Object.assign({}, this.state.asesoriasPorMes);
-
-        dataMonth[this.MONTHS[date.getMonth()]] = Object.assign({}, this.dataDefault);
-
-        // console.log(dataMonth[this.MONTHS[date.getMonth()]]);
-
-        dataMonth[this.MONTHS[date.getMonth()]].datasets[0].data[date.getDate()-1] += 1;
-        dataMonth[this.MONTHS[date.getMonth()]].mes = this.MONTHS[date.getMonth()];
-        let solicitadas = this.state.solicitadasPorMes;
-        solicitadas[date.getMonth()] += 1;
-        this.setState({solicitadasPorMes: solicitadas});
-        
-        if(asesoria.data().status === 'finalizada') {
-          this.setState({concretadas: this.state.concretadas + 1})
-          dataMonth[this.MONTHS[date.getMonth()]].datasets[1].data[date.getDate()-1] += 1;
-        }
-        if(asesoria.data().status === 'cancelada') {
-          this.setState({canceladas: this.state.canceladas + 1})
-          dataMonth[this.MONTHS[date.getMonth()]].datasets[2].data[date.getDate()-1] = this.state.canceladas;
-        }
-        
-        this.setState({asesoriasPorMes:dataMonth});
-        // console.log(this.state.mesSolicitado, this.state.asesoriasPorMes[this.state.mesSolicitado]);
-        // console.log(dataMonth);
-        dataMonth = [];
-        // this.setState({asesoriasPorMes: update(this.state.asesoriasPorMes, {[date.getMonth()]:})})
-      });
     });
   }
 
-  handleMonthChange = e => {
+  getRange = (month) => {
+    month = this.MONTHS.indexOf(month);
+    month += 1;
+    const now = new Date();
+    const from = new Date(`${now.getFullYear()}-${month > 10 ? month : '0'+month}`)
+    month += 1;
+    const to = new Date(`${now.getFullYear()}-${month > 10 ? month : '0'+month}`)
+    console.log("From:", from,"\nTo:", to);
+    
+    let ref = this.props.firebase.db.collection('asesorias')
+    ref = ref.where('fechaDeSolicitud', '>', from);
+    ref = ref.where('fechaDeSolicitud', '<', to);
+    
+    ref.onSnapshot(doc => {
+      this.setState({ totalMes: doc.size, canceladas: 0, concretadas: 0 });
+      // console.log(doc);
+      let asesoriasPorMes = {
+        labels: [1, 2, 3, 4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31],
+        solicitadas: 0,
+        datasets: [
+          {
+            label: 'Asesorías Solicitadas',
+            backgroundColor: 'rgba(181,93,232,0.2)',
+            borderColor: 'rgba(181,93,232,1)',
+            borderWidth: 1,
+            hoverBackgroundColor: 'rgba(181,93,232,0.4)',
+            hoverBorderColor: 'rgba(181,93,232,1)',
+            data: [0,0,0,0,0,0,0,0,0,0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+          },
+          {
+            label: 'Asesorías Concretadas',
+            backgroundColor: 'rgba(68,144,255,0.2)',
+            borderColor: 'rgba(68,144,255,1)',
+            borderWidth: 1,
+            hoverBackgroundColor: 'rgba(68,144,255,0.4)',
+            hoverBorderColor: 'rgba(68,144,255,1)',
+            data: [0,0,0,0,0,0,0,0,0,0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+          } ,
+          {
+            label: 'Asesorías No Concretadas',
+            backgroundColor: 'rgba(255,99,132,0.2)',
+            borderColor: 'rgba(255,99,132,1)',
+            borderWidth: 1,
+            hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+            hoverBorderColor: 'rgba(255,99,132,1)',
+            data: [0,0,0,0,0,0,0,0,0,0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+          } 
+        ]
+      };
+      doc.forEach(asesoria => {
+        asesoria = asesoria.data();
+        let fecha = new Date(asesoria.fechaDeSolicitud.seconds * 1000);
+        
+        asesoriasPorMes.datasets[0].data[fecha.getDate()-1] += 1;
+        if(asesoria.status === "cancelada" || asesoria.status === "pendiente") {
+          this.setState({canceladas: this.state.canceladas + 1});
+        }
+        if(asesoria.status === "finalizada") {
+          this.setState({concretadas: this.state.concretadas + 1});
+        }
+      });
+      
+      this.setState({asesoriasPorMes});
+      console.log(asesoriasPorMes);
+      asesoriasPorMes = {};
+    });
+  }
+
+  handleMonthChange = async e => {
     e.preventDefault();
-    this.setState({ mesSolicitado: e.target.value });
-    console.log(this.state.asesoriasPorMes);
+    await this.setState({ mesSolicitado: e.target.value });
+    this.getRange(this.state.mesSolicitado);
+    // console.log(this.state.mesSolicitado);
   }
 
   render() {
@@ -104,28 +109,28 @@ class RepAsesorias extends React.Component {
         <article className="col-xs-6 col-md-3">
           <div className="card active bradius card-dash" >
             <p className="gray card-dash--title">Total de asesorías solicitadas</p>
-            <p className="card-dash--value bold xx-large">{this.state.total}</p>
+            <p className="card-dash--value bold xx-large">{this.state.total | 0}</p>
           </div>
         </article>
 
         <article className="col-xs-6 col-md-3">
           <div className="card active bradius card-dash" >
-            <p className="gray card-dash--title">Asesorías concretadas</p>
-            <p className="card-dash--value bold xx-large">{Math.round(100*(this.state.concretadas/this.state.total)*100)/100}%</p>
+            <p className="gray card-dash--title">Concretadas en el mes de {this.state.mesSolicitado}</p>
+            <p className="card-dash--value bold xx-large">{Math.round(100*(this.state.concretadas/this.state.totalMes)*100)/100 | 0}%</p>
           </div>
         </article>
         
         <article className="col-xs-6 col-md-3">
           <div className="card active bradius card-dash" >
-            <p className="gray card-dash--title">Asesorías no concretadas</p>
-            <p className="card-dash--value bold xx-large">{Math.round(100*(this.state.canceladas/this.state.total)*100)/100}%</p>
+            <p className="gray card-dash--title">Concretadas en el mes de {this.state.mesSolicitado}</p>
+            <p className="card-dash--value bold xx-large">{Math.round(100*(this.state.canceladas/this.state.totalMes)*100)/100 | 0}%</p>
           </div>
         </article>
 
         <article className="col-xs-6 col-md-3">
           <div className="card active bradius card-dash" >
             <p className="gray card-dash--title">Asesorías solicitadas en {this.state.mesSolicitado}</p>
-            <p className="card-dash--value bold xx-large">{this.state.solicitadasPorMes[this.MONTHS.indexOf(this.state.mesSolicitado)]}</p>
+            <p className="card-dash--value bold xx-large">{this.state.totalMes | 0}</p>
           </div>
         </article>
 
@@ -134,13 +139,13 @@ class RepAsesorias extends React.Component {
             <p className="gray card-dash--title">Asesorías por día del mes de {this.state.mesSolicitado}</p>
             <select className="card-dash--select" value={this.state.mesSolicitado} onChange={this.handleMonthChange} >
               { 
-                Object.keys(this.state.asesoriasPorMes).map((key, _$) =>
-                  <option value={key}>{key}</option>
+                this.MONTHS.map(val =>
+                  <option value={val}>{val}</option>
                 )
               }
             </select>
             <Bar 
-              data={this.state.asesoriasPorMes[this.state.mesSolicitado]} />
+              data={this.state.asesoriasPorMes} />
             {/* <Line 
               data={this.state.asesoriasPorMes[this.state.mesSolicitado]} /> */}
           </div>
